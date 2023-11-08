4e65143c4b4c6ab09b037d4742c3d36b17617b93
Elisio Ricardo <elisio.silva@sysmap.com>
2023-11-08 16:43:32 -0300

    adicionando implementação findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,8 +17,17 @@ public class Program {
         //Dessa forma o programa não conhece a classe de implemetação apenas a interface
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=========== Teste 1 ===============");
         Seller sel = sellerDao.findById(3);
         System.out.println(sel);
+
+        System.out.println("=========== Teste 2 ===============");
+        Department department2 = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department2);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
 
     }
 }
